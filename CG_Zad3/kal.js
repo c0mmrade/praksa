32c09be71093ff8c054b2f159a3ca8b78f8b1cbe
JovanaT99 <jovanablagojevic98@gmail.com>
2020-06-08 14:11:05 +0200

    Petak

---
+++ CG_Zad3/kal.js
@@ -0,0 +1,21 @@
+function Mnozi()
+{
+
+	var x=document.getElementById("num1").value;
+	var y=document.getElementById("num").value;
+	var z=x*y;
+
+	document.getElementById("fun").innerHTML ="Proizod brojeva je:" + z;
+}
+
+function Deli()
+{
+
+	var x=document.getElementById("num1").value;
+	var y=document.getElementById("num").value;
+	var z=x/y;
+
+	document.getElementById("fun").innerHTML = "Kolicnik brojeva je :" + z;
+}
+
+
