6623e5e7d543c9f4aa9c7c6011314604f61b7ab5
JovanaT99 <66609488+JovanaT99@users.noreply.github.com>
2020-09-28 16:40:34 +0200

    Update Hkula.java

--- Hkula.java
+++ Hkula.java
@@ -14,7 +14,7 @@ package jovana;
 			if(brojDiskova<1 || brojDiskova>10) {
 				System.out.println("Broj diskova ne moze biti manji od 1 ili veci od 10");
 				return;
-			
+				//Odnosi se na manji disk koji je na vrhu
 			}  if(brojDiskova==1) {
 				
 				System.out.println("Staviti disk 1 sa " + prvi + " " +"na"+ " "+ treci);
@@ -24,24 +24,20 @@ package jovana;
 			hanojiKula(brojDiskova-1,prvi,drugi,treci);
 			System.out.println("Staviti disk " + brojDiskova + " "+ "sa" + " " + prvi + " "+"na" + " "+treci);
 			hanojiKula(brojDiskova-1,drugi,prvi,treci);
+				
+			}
+			public  static void main(String[]args) {
 			
-			
-		}
-
-		public  static void main(String[]args) {
-			
-            Scanner ulaz=new Scanner(System.in);
-
-			System.out.println("Unesite broj diskova:");
+            		Scanner ulaz=new Scanner(System.in);
+			System.out.println("Unesite broj diskova od 1-10:");
 		
-			
 			int brojDiskova=ulaz.nextInt();
 			//Po formuli sa Wikipedije najmanji broj poteza se racuna po formuli 2 na n-1
 			System.out.print("Najmanji broj poteza koje mozete sa " +brojDiskova+ " disk-a"+ " " +"je:"+" " +(Math.pow(2,brojDiskova)-1)+"\n");
 			//Za blanko liniju
 			System.out.println();
 			hanojiKula(brojDiskova,"stapPrvi" ,"stapDrugi", "StapTreci");		
-	}
-	}
+			}
+			}
 
 	
