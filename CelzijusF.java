5ff2cc74810876fd5cb65eff24d12a616de024ab
JovanaT99 <jovanablagojevic98@gmail.com>
2020-09-29 14:05:51 +0200

    Celzjus

---
+++ CelzijusF.java
@@ -0,0 +1,19 @@
+package jovana;
+
+import java.util.Scanner;
+
+public class CelzijusiFaranhajt {
+	public static void main(String[]args){
+
+double faranhajt;
+int celzijus;
+
+Scanner tastatura=new Scanner(System.in);
+
+System.out.print("Unesite stepene u Celzijusima:");
+celzijus=tastatura.nextInt();
+
+System.out.print( faranhajt=9*celzijus/5+32);
+ 
+}
+}
